a4c892e4c78dcfb8214775f70a4d8b99808853bf
DaniKaizenNetwork <danielitasanabria10@gmail.com>
2025-09-22 13:25:25 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'layoutBasica',
+  webDir: 'dist'
+};
+
+export default config;
